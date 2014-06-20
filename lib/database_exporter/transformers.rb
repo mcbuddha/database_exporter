ba5036a4566bb66b2e7f19653f63259573193ff3
Marton Somogyi <mcbuddha@gmail.com>
2014-06-20 14:58:35 +0200

    Rename transformers

--- lib/database_exporter/transformers.rb
+++ lib/database_exporter/transformers.rb
@@ -1,10 +1,10 @@
 module DatabaseExporter
   Transformers = {
-    'sanitize_email' => ->(i, rec) { "email#{i.to_s.rjust(5, ?0)}@#{rec.split(?@)[1]}"},
+    'email' => ->(i, rec) { "email#{i.to_s.rjust(5, ?0)}@#{rec.split(?@)[1]}"},
     'wipe' => proc { nil },
     'zero' => proc { 0 },
     'empty_string' => proc { '' },
-    'sanitize_name' => proc { 'John Doe' },
-    'sanitize_phone_number' => ->(i, rec) { rec.nil? ? rec : "#{rec[0,3]}#{i.to_s.rjust rec.length-3, ?0}" }
+    'name' => proc { 'John Doe' },
+    'phone_number' => ->(i, rec) { rec.nil? ? rec : "#{rec[0,3]}#{i.to_s.rjust rec.length-3, ?0}" }
   }
 end
